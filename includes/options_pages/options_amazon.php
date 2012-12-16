ed6678771fc97c007a09b2cdb9d0a80288ac7e14
sealsystems <sealsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-16 10:10:02 +0000

    Additional files for 5.0
    git-svn-id: https://plugins.svn.wordpress.org/app-store-assistant/tags/5.0@639971 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/options_pages/options_amazon.php
@@ -0,0 +1,5 @@
+<h2 class="asa_admin">Amazon.com Settings:</h2>
+		<div class="asa_admin">
+		<p><b>This feature is coming soon...<b></p>
+		Amazon.com Affiliate Code: <input type="text" size="50" name="appStore_options[amazoncode]" value="<?php echo $options['amazoncode']; ?>"/>
+		</div>
