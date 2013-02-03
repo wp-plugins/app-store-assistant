60304a5eda1f03b01b103f8e5b8afcffe034046e
sealsystems <sealsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-03 09:21:10 +0000

    Version 5.6.4
    git-svn-id: https://plugins.svn.wordpress.org/app-store-assistant/tags/5.6.4@662740 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/options_pages/options_reset.php
@@ -0,0 +1,12 @@
+		<input type="hidden" name="appStore_options[checkboxedoptions]" value="ResetCheckOne,ResetCheckTwo,ResetCheckThree,ResetCacheOne,ResetCacheTwo" />
+<h2 class="asa_admin">Reset All Settings to Defaults:</h2>
+		<div class="asa_admin">
+		<input type="checkbox" value="DoIt" name="appStore_options[ResetCheckOne]" /> I want to reset all settings to their defaults.<br /><br />
+		<input type="checkbox" value="DoIt" name="appStore_options[ResetCheckTwo]" /> I know this will not save any of my changes.<br /><br />
+		<input type="checkbox" value="DoIt" name="appStore_options[ResetCheckThree]" /> I wont get mad when all my changes are lost.<br /><br />
+		</div>
+		<h2 class="asa_admin">Reset cached App data:</h2>
+		<div class="asa_admin">
+		<input type="checkbox" value="DoIt" name="appStore_options[ResetCacheOne]" /> I want to reset all cached app data.<br /><br />
+		<input type="checkbox" value="DoIt" name="appStore_options[ResetCacheTwo]" /> I know this will slow down my site as it rebuilds.
+		</div>
