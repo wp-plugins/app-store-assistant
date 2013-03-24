3fe7dbf1e12d57692123c26f10bb5f9fb3c41bb7
sealsystems <sealsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-24 19:08:47 +0000

    = 6.0.2 = * Fixed: An issue where the ios_app_elements shortcode was returning data out of order  [Thanks beakernet]
    git-svn-id: https://plugins.svn.wordpress.org/app-store-assistant/trunk@686696 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app-store-assistant.php
+++ app-store-assistant.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: App Store Assistant
-Version: 6.0.0
+Version: 6.0.2
 Text Domain: appStoreAssistant
 Plugin URI: http://TheiPhoneAppsList.com/
 Description: Adds shortcodes to display ATOM feed or individual item information from Apple's App Stores or iTunes. Now works with Amazon.com Affiliate Program.
