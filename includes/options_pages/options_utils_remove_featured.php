cd9ae1e743a2bfb71b55a30ab3eac0c18204d04c
sealsystems <sealsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-27 23:34:19 +0000

    = 6.5.1 = * Fix: Minor bugs fixed
    = 6.5.0 =
    * Changed: New simplified method of choosing which elements to display [IMPORTANT: CHECK YOUR SETTINGS]
    * Added: Description of the "Assisted By" link added to Main Page
    * Changed: The "Assisted By" link is off by default
    * Added: Italian Translation [Thanks to Angelo Furnò (giankikine for the web ;)]
    * Changed: Completely replaced and optimized code to Rebuild Featured Images
    * Added: Additional frequently used items to + New Post menu
    * Added: Omnisearch results, your visitors can now search for any app from the app store. (optional)
    * Updated: Lightbox 2.6 <http://lokeshdhakar.com/projects/lightbox2/>
    * Added: Ability to not include Lightbox (Enabled by default). May be included in other plugins.
    * Changed: Accepted jpeg (as opposed to jpg) for images (did not work with caching system)
    * Removed: References to discontinued Affiliate programs Linkshare & DGM
    * Fixed: Code to remove Featured Images from older versions
    * Added: Now removes all Featured Images set by this plugin
    * Changed: Nicer buttons with shadow
    * Fixed: iosUniversal not displaying properly
    * Fixed: Various PHP 5 strict errors
    * Fixed: Widget icon not showing
    * Fixed: Widget icon not respecting size set in preferences
    * Changed: Widget now uses same button colors & style as posts
    * Added: You can now change the number of apps displayed in widget
    * Fixed: Searching for Mac apps (Actually this was fixed by Apple)
    * Changed: Adjusted size of icon in search results to allow for new larger icons
    * Changed: Optimized code to cache images
    * Changed: Changed file names for cached images
    * Changed: Search/Add now uses new shortcode
    * Added: Check for writeable directory during featured image creation
    * Changed: Featured Images are saved in the cache folder sized to theme specs
    * Fixed: If original item image was too small, various sizes were not created
    * Added: Price now available in Details list
    * Added: Separate App Icon with Buy Button now available
    * Added: Inline elements as option for Single and Multiple posts
    
    git-svn-id: https://plugins.svn.wordpress.org/app-store-assistant/tags/6.5.1@829280 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/options_pages/options_utils_remove_featured.php
@@ -0,0 +1,9 @@
+<input type="hidden" name="appStore_options[checkboxedoptions]" value="ResetFIOne,ResetFITwo" />
+<p class="asa_admin_warning"><b>This option will Remove Featured Images that were created using an old version of this plugin. Featured images that you set manually, will not be changed.</b> You may need to run this process a few times depending on how many posts you have.</p>
+<p>You may also use this version to remove all Featured Images created with this plugin.</p>
+<p>After removing the Featured Images, visit the <a href="admin.php?page=appStore_sm_utilities&tab=defaultTab">Add Featured Images</a> tab to add the new format Featured Images to your posts.</p>
+
+<div class="asa_admin">
+	<input type="checkbox" value="DoIt" name="appStore_options[ResetFIOne]" /> I want to remove all ASA set Featured Images.<br /><br />
+	<input type="checkbox" value="DoIt" name="appStore_options[ResetFITwo]" /> I know this will slow down my site as it rebuilds.
+</div>
