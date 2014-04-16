edfbc2245cbedb389bd0b7c14c0007db1e2ffd1f
sealsystems <sealsystems@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 23:15:04 +0000

    = 6.6.4 = * Added: New Dialog boxes for full control of shortcodes in Editor * Changed: Updated for Wordpress 3.9 & Tiny MCE 4.0 * Added: Amazon.com shortcode creator to Editor
    = 6.6.3 =
    * Changed: If caching is turned off, a width & height parameter is added to img tags for icons [Thanks tamansu]
    * Changed: Turned off UTF-8 Encoding of results from Apple Search API.
    
    = 6.6.2 =
    * Fixed: Featured Image rebuild needed an clear cache
    * Fixed: Not finding posts with new shortcodes when doing app search [Thanks Aslan Guseinov]
    * Fixed: Screenshot settings now work on pages with Multiple Posts  [Thanks Aslan Guseinov]
    
    = 6.6.1 =
    * Added: Dashboard widget "Search for apps"
    * Added: You can now search for an app by name or App ID [Thanks Aslan Guseinov]
    * Added: Featured Images now work if cache is disabled
    * Added: Add missing categories to ASA posts [Thanks Aslan Guseinov]
    * Fixed: Featured Image was set to a size of 1x1
    * Changed: Featured Image functions replaced
    * Changed: Cleaned up search form and results
    * Fixed: Links in WP Admin Bar
    * Fixed: ASA Excerpt Builder now processes shortcodes with link instead of id
    * Changed: Moved Rebuild Featured Images to Rebuild menu
    * Added: I18n aka Localization is supported via POT file for admin area
    
    = 6.6.0 =
    * Added: Added listing of Tracks for Albums [Thanks kittyj]
    * Added: Tracks names now link to Track preview
    * Added: Now checks for proper PHP version (5.4 or greater)
    * Added: Warning for older PHP versions
    * Added: You can now specify max width and max height for app icons or amazon.com items
    * Added: You can now specify to crop or keey aspect ratio for app icons or amazon.com items
    * Added: Documentation for Amazon.com affiliate program to help section
    * Added: You can now set the Max size for iPad & iPhone screenshots [Thanks tkrones]
    * Added: You can now set the Max size for Amazon.com items
    * Fixed: Amazon.com cached images not displaying after update
    * Added: ProductGroup eBooks for Amazon.com items
    * Fixed: Issues with RSS feed not displaying app info
    * Added: Additional error checking for Amazon.com responses
    * Fixed: Unchecking "Enable Lightbox" would not save preference [Thanks Jomasher]
    * Fixed: Elements not displaying [Thanks Jomasher]
    * Fixed: Issue with "&#65533;" character displaying in Amazon.com descriptions
    * Changed: Cleaned up text coming from Amazon.com feeds
    * Fixed: Issue with conflicting functions [Thanks JacobN]
    * Fixed: Issue displaying Rating Info if there were now ratings (zero stars)
    * Fixed: Issue with RSS feed not respecting settings for short description or icon
    * Fixed: Issue with Album of Software
    
    git-svn-id: https://plugins.svn.wordpress.org/app-store-assistant/tags/6.6.4@895311 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/options_pages/options_general_defaultTab.php
+++ includes/options_pages/options_general_defaultTab.php
@@ -1,24 +1,138 @@
-<input type="hidden" name="appStore_options[checkboxedoptions]" value="use_shortDesc_on_single,use_shortDesc_on_multiple" />
-<h2 class="asa_admin">Short Description</h2>
+<input type="hidden" name="appStore_options[checkboxedoptions]" value="displayLinkToFooter,cache_images_locally" />
+
+<?php
+if (version_compare(phpversion(), '5.4', '<')) {
+    // php version isn't high enough
+?>
+<h2 class="asa_admin">Warning!</h2>
+	<div class="asa_admin">
+		<div class="asa_admin_element">
+			<p class="asa_admin_warning">Warning: Your PHP version of <?php echo phpversion(); ?> is below the required version 5.4.</p>
+			<p>Some features may not work correctly. It is recommended that you upgrade to a current version.</p>
+
+		</div>
+	</div>
+<?php
+}
+?>
+
+
+
+<h2 class="asa_admin">Setup</h2>
+	<div class="asa_admin">
+		<div class="asa_admin_element">
 		
-<table class="form-table">
-<tr valign="top">
-<th scope="row"><label>Short Description</label></th>
-<td><input type="text" size="4" name="appStore_options[max_description]" value="<?php echo $options['max_description']; ?>" maxlength="4" />  Max characters<br />
-<input type="checkbox" name="appStore_options[use_shortDesc_on_single]" value="yes" <?php if ($options['use_shortDesc_on_single'] == "yes") echo 'checked'; ?> /> Use on pages with a Single Post<br />
-<input type="checkbox" name="appStore_options[use_shortDesc_on_multiple]" value="yes" <?php if ($options['use_shortDesc_on_multiple'] == "yes") echo 'checked'; ?> /> Use on pages with Multiple Posts<br />
-<input type="checkbox" name="appStore_options[use_shortDesc_on_atomfeed]" value="yes" <?php if ($options['use_shortDesc_on_atomfeed'] == "yes") echo 'checked'; ?> /> Use on pages with an ATOM Feed or List of Apps
-</td>
-</tr>
-<tr valign="top">
-<th scope="row"><label>Read More link</label></th>
-<td><input type="radio" name="appStore_options[shortDesc_link]" value="button" <?php if ($options['shortDesc_link'] == "button") echo 'checked'; ?> /> show as button<br />
-<input type="radio" name="appStore_options[shortDesc_link]" value="text" <?php if ($options['shortDesc_link'] == "text") echo 'checked'; ?> /> show as text<br />
-<input type="radio" name="appStore_options[shortDesc_link]" value="hide" <?php if ($options['shortDesc_link'] == "hide") echo 'checked'; ?> /> hide link</td>
-</tr>
-<tr valign="top">
-<th scope="row"><label>Default Text</label></th>
-<td><input type="text" size="20" name="appStore_options[shortDesc_screenshot_text]" value="<?php echo $options['shortDesc_screenshot_text']; ?>" maxlength="30" /> "Show Screenshots"<br />
-<input type="text" size="35" name="appStore_options[shortDesc_fullDesc_text]" value="<?php echo $options['shortDesc_fullDesc_text']; ?>" maxlength="45" /> "Show Full Description"</td>
-</tr>
-</table>	
+	<section>
+	  <h3>Show link to plugin site in footer</h3>
+	  	<div class="checkboxOne">
+	  		<input type="checkbox" value="yes" id="checkboxOneInput" name="appStore_options[displayLinkToFooter]" <?php if ($options['displayLinkToFooter'] == "yes") echo 'checked'; ?> />
+		  	<label for="checkboxOneInput"></label>
+	  	</div>
+	</section>
+		
+<p class="asa_admin_warning">By selecting the above box, a link will be placed at the bottom of your WordPress site giving credit to this plugin. This link will contain the page that it is displayed on and the version number of the plugin. When the link is clicked it will share this information with our server. The information will allow us to better understand how the plugin is being used and to make further improvements. This is completely optional, and the plug-in will work just fine even if you don't select this option. but we ask that you do select it. If you reset your settings it will be off by default.</p>
+		<p>An Example link for this site would be <?php echo 'http://theiphoneappslist.com/index.php?v='.urlencode(plugin_get_version())."&ac=".urlencode(appStore_setting('affiliatepartnerid')).'&link='.urlencode(site_url()); ?></p></div>
+	</div>
+	
+<h2 class="asa_admin">Localization</h2>
+	<div class="asa_admin">
+		<div class="asa_admin_element">Currency Type: <select name='appStore_options[currency_format]'>
+			<option value="USD" <?php if ($options['currency_format'] == "USD") echo 'selected'; ?>>US ($ and &cent;)</option>
+			<option value="EUR" <?php if ($options['currency_format'] == "EUR") echo 'selected'; ?>>Euro (&euro;)</option>
+			<option value="GBP" <?php if ($options['currency_format'] == "GBP") echo 'selected'; ?>>United Kingdom Pound (&pound;)</option>
+			<option value="NOK" <?php if ($options['currency_format'] == "NOK") echo 'selected'; ?>>Norway Krone (kr)</option>
+			<option value="SEK" <?php if ($options['currency_format'] == "SEK") echo 'selected'; ?>>Sweden Krona (kr)</option>
+			<option value="JPY" <?php if ($options['currency_format'] == "JPY") echo 'selected'; ?>>Japan Yen &yen;</option>
+		</select></div>
+		<div class="asa_admin_element">
+			Show results from this country's store: <select name='appStore_options[store_country]'>
+				<?php
+					$storeCountries = array("US","AT","BE","CH","DE","DK","EE","ES","FI","FR","GB","IE","IT","NL","NO","SE");
+					foreach($storeCountries as $storeCountry) {
+						echo '<option value="'.$storeCountry.'" ';
+						if ($options['store_country'] == $storeCountry) echo 'selected'; 
+						echo '>iTunes '.$storeCountry.'</option>'."\r\n";
+					
+					}
+				?>
+				</select><br />
+			Show results using this language: <select name='appStore_options[store_language]'>
+			<option value="us" <?php if ($options['store_language'] == "us") echo 'selected'; ?>>English</option>
+			<option value="CZ" <?php if ($options['store_language'] == "CZ") echo 'selected'; ?>>Čeština</option>
+			<option value="DE" <?php if ($options['store_language'] == "DE") echo 'selected'; ?>>Deutsch</option>
+			<option value="ES" <?php if ($options['store_language'] == "ES") echo 'selected'; ?>>Español</option>
+			<option value="ESLA_MX" <?php if ($options['store_language'] == "ESLA_MX") echo 'selected'; ?>>Español Latino</option>
+			<option value="FR" <?php if ($options['store_language'] == "FR") echo 'selected'; ?>>Français</option>
+			<option value="IT" <?php if ($options['store_language'] == "IT") echo 'selected'; ?>>Italiano</option>
+			<option value="NO" <?php if ($options['store_language'] == "NO") echo 'selected'; ?>>Norsk</option>
+			<option value="PL" <?php if ($options['store_language'] == "PL") echo 'selected'; ?>>Polski</option>
+			<option value="ru" <?php if ($options['store_language'] == "ru") echo 'selected'; ?>>Pусский</option>
+			<option value="FI" <?php if ($options['store_language'] == "FI") echo 'selected'; ?>>Suomi</option>
+			<option value="SE" <?php if ($options['store_language'] == "SE") echo 'selected'; ?>>Svenska</option>
+			<option value="FI" <?php if ($options['store_language'] == "FI") echo 'selected'; ?>>Tagalog</option>
+			<option value="AR" <?php if ($options['store_language'] == "AR") echo 'selected'; ?>>العربية</option>
+			<option value="KR" <?php if ($options['store_language'] == "KR") echo 'selected'; ?>>한국어</option>
+			<option value="JP" <?php if ($options['store_language'] == "JP") echo 'selected'; ?>>日本語</option>
+			<option value="CN" <?php if ($options['store_language'] == "CN") echo 'selected'; ?>>简体中文</option>
+			<option value="HK_TW" <?php if ($options['store_language'] == "HK_TW") echo 'selected'; ?>>繁體中文</option>
+				</select><br />
+		</div>
+				<p class="asa_admin_warning">(Cached app data must be cleared for change to take effect. See <b><a href="<?php echo admin_url()."admin.php?page=appStore_sm_utilities&tab=clearcache"; ?>">Utilities -> Clear Cache</a></b>.)</p>
+		<div class="asa_admin_element">iTunes/App Stores Badge Language: <select name='appStore_options[store_badge_language]'>
+			<option value="US-UK" <?php if ($options['store_badge_language'] == "US-UK") echo 'selected'; ?>>English</option>
+			<option value="CZ" <?php if ($options['store_badge_language'] == "CZ") echo 'selected'; ?>>Čeština</option>
+			<option value="DE" <?php if ($options['store_badge_language'] == "DE") echo 'selected'; ?>>Deutsch</option>
+			<option value="ES" <?php if ($options['store_badge_language'] == "ES") echo 'selected'; ?>>Español</option>
+			<option value="ESLA_MX" <?php if ($options['store_badge_language'] == "ESLA_MX") echo 'selected'; ?>>Español Latino</option>
+			<option value="FR" <?php if ($options['store_badge_language'] == "FR") echo 'selected'; ?>>Français</option>
+			<option value="IT" <?php if ($options['store_badge_language'] == "IT") echo 'selected'; ?>>Italiano</option>
+			<option value="NO" <?php if ($options['store_badge_language'] == "NO") echo 'selected'; ?>>Norsk</option>
+			<option value="PL" <?php if ($options['store_badge_language'] == "PL") echo 'selected'; ?>>Polski</option>
+			<option value="RU" <?php if ($options['store_badge_language'] == "RU") echo 'selected'; ?>>Pусский</option>
+			<option value="FI" <?php if ($options['store_badge_language'] == "FI") echo 'selected'; ?>>Suomi</option>
+			<option value="SE" <?php if ($options['store_badge_language'] == "SE") echo 'selected'; ?>>Svenska</option>
+			<option value="FI" <?php if ($options['store_badge_language'] == "FI") echo 'selected'; ?>>Tagalog</option>
+			<option value="AR" <?php if ($options['store_badge_language'] == "AR") echo 'selected'; ?>>العربية</option>
+			<option value="KR" <?php if ($options['store_badge_language'] == "KR") echo 'selected'; ?>>한국어</option>
+			<option value="JP" <?php if ($options['store_badge_language'] == "JP") echo 'selected'; ?>>日本語</option>
+			<option value="CN" <?php if ($options['store_badge_language'] == "CN") echo 'selected'; ?>>简体中文</option>
+			<option value="HK_TW" <?php if ($options['store_badge_language'] == "HK_TW") echo 'selected'; ?>>繁體中文</option>
+		</select></div>
+	</div>
+		
+<h2 class="asa_admin">Cache Settings</h2>
+   	<div class="asa_admin">
+		<div class="asa_admin_element">
+			<section>
+			  <h3>Cache images locally</h3>
+				<div class="checkboxTwo">
+					<input type="checkbox" value="1" id="checkboxTwoInput" name="appStore_options[cache_images_locally]" <?php if ($options['cache_images_locally'] == "1") echo 'checked'; ?> />
+					<label for="checkboxTwoInput"></label>
+				</div>
+			</section>
+			<p class="asa_admin_explain">This will load icons, screenshots, etc. from this server instead of using Apple's CDN server.<br />The <b><?php echo $upload_dir['basedir']; ?></b> directory MUST be writeable for this option to have any effect.</p>
+			<p class="asa_admin_warning">(Cached app data must be cleared for change to take effect. See <b><a href="<?php echo admin_url()."admin.php?page=appStore_sm_utilities&tab=clearcache"; ?>">Utilities -> Clear Cache</a></b>.)</p>
+
+		</div>
+    	<div class="asa_admin_element">Data cache time: <select name='appStore_options[cache_time_select_box]'>
+			<?php $cache_intervals = array(
+										'Don\'t cache'=>0,
+										'1 minute'=>1*60,
+										'5 minutes'=>5*60,
+										'10 minutes'=>10*60,
+										'30 minutes'=>30*60,
+										'1 hour'=>1*60*60,
+										'6 hours'=>6*60*60,
+										'12 hours'=>12*60*60,
+										'24 hours'=>24*60*60,
+										'1 Week'=>24*60*60*7,
+										'1 Month'=>24*60*60*7*30,
+										'1 Year'=>24*60*60*7*30*365
+									);
+			
+			foreach ($cache_intervals as $key => $value) {
+				echo '<option value="' . $value . '" ' . selected($value, $options['cache_time_select_box']) . '>' . $key . '</option>';
+			}?></select><br />
+		<p class="asa_admin_explain">This option determines how long before the plugin requests new data from Apple's servers. You can clear the cached app data via the Utilities section.</p>
+		</div>
+	</div>
--- includes/options_pages/options_help_shortcodes.php
+++ includes/options_pages/options_help_shortcodes.php
@@ -1,42 +1,38 @@
+<h2>Item IDs</h2>
+<h3 class="asa_admin">iOS App ID:</h3>
+<i>https://itunes.apple.com/us/app/1password/id<b>568903335</b>?mt=8</i><br />
+Works with iOS Apps or Apple TV Apps
+<h3 class="asa_admin">iTunes Item:</h3>
+<i>https://itunes.apple.com/us/album/aliens-original-motion-picture/id<b>62101268</b></i><br />
+Works with Songs, Albums, Movies, Short Films, TV Episodes, Seasons, AudioBooks, eBooks and Music Videos
+<h3 class="asa_admin">Mac App ID:</h3>
+<i>https://itunes.apple.com/us/app/1password-password-manager/id<b>443987910</b>?mt=12</i><br />
+Works with Mac Apps
+<h3 class="asa_admin">Amazon.com ASIN:</h3>
+The Amazon Standard Identification Number is a 10-character alphanumeric unique identifier assigned by Amazon.com<p>Found in the URL from Amazon.com<br />For example: <i>http://www.amazon.com/dp/<b>B001KNH8VU</b>/?tag=047-20</i></p>
+
+<hr>
 <h2>Shortcodes</h2>
 
 <h3 class="asa_admin">Display a single item:</h3>
 
 <div class="asa_help">
 <ul>
-<li>[ios_app id="568903335" more_info_text="More Info on this App..."]<p>Displays a single iOS app</p>
+<li>[asa_item id="568903335" more_info_text="More Info on this App..."]<p>Displays a single Mac/iOS app or iTunes item</p>
 	<ul>
-	<li>id: The iOS app's App Store ID<p>This can be found in the copied link from the iTunes App Store.<br />For example: <i>https://itunes.apple.com/us/app/1password/id<b>568903335</b>?mt=8</i></p></li>
-	<li>more_info_text: Excerpt "more info" link text (optional)</li>
-	</ul>
-</li>
-<li>[mac_app id="443987910" more_info_text="More Info on this Mac App..."]<p>Displays a single Mac app</p>
-	<ul>
-	<li>id: The Mac App Store ID<p>This can be found in the copied link from the App Store.<br />For example: <i>https://itunes.apple.com/us/app/1password-password-manager/id<b>443987910</b>?mt=12</i></p></li>
-	<li>more_info_text: Excerpt "more info" link text (optional)</li>
-	</ul>
-</li>
-<li>[itunes_store id="62101268" more_info_text="More Info on this iTunes item..."]<p>Displays a single item from the iTunes Store</p>
-	<ul>
-	<li>id: The iTunes Store ID<p>This can be found in the copied link from the iTunes Store.<br />For example: <i>https://itunes.apple.com/us/album/aliens-original-motion-picture/id<b>62101268</b></i><br />Works with Songs, Albums, Movies, Short Films, TV Episodes, Seasons and Music Videos</p></li>
+	<li>id: The Items Store ID<</li>
 	<li>more_info_text: Excerpt "more info" link text (optional)</li>
 	</ul>
 </li>
 <li>[amazon_item asin="B001KNH8VU" more_info_text="More Info on this item from Amazon.com..."]<p>Displays a single item from Amazon.com</p>
 	<ul>
-	<li>asin: The Amazon Standard Identification Number is a 10-character alphanumeric unique identifier assigned by Amazon.com<p>Found in the URL from Amazon.com<br />For example: <i>http://www.amazon.com/dp/<b>B001KNH8VU</b>/?tag=047-20</i></p></li>
+	<li>asin: The Amazon Standard Identification Number</li>
 	<li>more_info_text: Excerpt "more info" link text (optional)</li>
 	</ul>
 </li>
-<li>[ios_app link="https://itunes.apple.com/us/app/1password/id568903335?mt=8" more_info_text="More Info on this App..."]<p>Displays a single iOS app using a link*</p>
+<li>[asa_item link="https://itunes.apple.com/us/app/1password/id568903335?mt=8" more_info_text="More Info on this App..."]<p>Displays a single Mac/iOS app or iTunes item using a link*</p>
 	<ul>
-	<li>link: The iOS app's App Store ID<p>This special shortcode was created for users of wp-o-matic.<br />For example: <i>https://itunes.apple.com/us/app/1password/id568903335?mt=8</i></p></li>
-	<li>more_info_text: Excerpt "more info" link text (optional)</li>
-	</ul>
-</li>
-<li>[mac_app link="https://itunes.apple.com/us/app/1password-password-manager/id443987910?mt=12" more_info_text="More Info on this Mac App..."]<p>Displays a single Mac app using a link*</p>
-	<ul>
-	<li>link: The Mac App Store ID<p>This special shortcode was created for users of wp-o-matic.<br />For example: <i>https://itunes.apple.com/us/app/1password-password-manager/id443987910?mt=12</i></p></li>
+	<li>link: The items full URL</li>
 	<li>more_info_text: Excerpt "more info" link text (optional)</li>
 	</ul>
 </li>
@@ -47,10 +43,9 @@
 <h3 class="asa_admin">Display several items from a ATOM/RSS Feed:</h3>
 	
 <ul>
-<li>[asaf_atomfeed atomurl="http://iTunes.apple.com/us/rss/toppaidmacapps/limit=25" mode="iOS" more_info_text="open in App Store..."]<p>Displays the items from the ATOM feed in a formatted view</p>
+<li>[asaf_atomfeed atomurl="http://iTunes.apple.com/us/rss/toppaidmacapps/limit=25" more_info_text="open in App Store..."]<p>Displays the items from the ATOM feed in a formatted view</p>
 	<ul>
 	<li>atomurl: feed URL supplied by Apple RSS Generator<p>These feeds can be generated here: <i>https://rss.itunes.apple.com/</i>.</p></li>
-	<li>mode: [iOS, Mac or iTunes]</li>
 	<li>more_info_text: Excerpt "more info" link text (optional)</li>
 	</ul>
 </li>
@@ -61,10 +56,9 @@
 	
 <ul>
 
-<li>[ios_app_list ids="568903335,62101268,443987910,568903335" more_info_text="open in App Store..." mode="iOS"]<p>Displays Several iOS apps on a single page or post</p>
+<li>[asa_list ids="568903335,62101268,443987910,568903335" more_info_text="open in Store..."]<p>Displays Several apps or iTunes items on a single page or post</p>
 	<ul>
-	<li>ids: Comma separated list iOS app's App Store IDs<p>These can be found in the copied link from the iTunes or Mac App Stores.<br />iOS App example: <i>https://itunes.apple.com/us/app/1password/id<b>568903335</b>?mt=8</i><br />iTunes example: <i>https://itunes.apple.com/us/album/aliens-original-motion-picture/id<b>62101268</b></i><br />Mac App example: <i>https://itunes.apple.com/us/app/1password-password-manager/id<b>443987910</b>?mt=12</i></p></li>
-	<li>mode: [iOS, Mac, Mixed or iTunes]<p>Mixed displays both iOS and Mac apps</p></li>
+	<li>ids: Comma separated list Mac App, iOS App's or iTunes IDs</li>
 	<li>more_info_text: Button text</li>
 	</ul>
 </li>
@@ -73,27 +67,14 @@
 <h3 class="asa_admin">Display just a html link to an item:</h3>
 
 <ul>
-	<li>[ios_app_link id="568903335" text="App Name"]<p>Displays a text only link to the iOS App</p>
+	<li>[asa_link id="568903335" text="App Name"]<p>Displays a text only link to the App or iTunes item</p>
 	<ul>
-	<li>id: The iOS app's App Store ID<p>This can be found in the copied link from the iTunes App Store.<br />For example: <i>https://itunes.apple.com/us/app/1password/id<b>568903335</b>?mt=8</i></p></li>
-	<li>text: Link text (optional)<p>If no text is specified, the item name will be displayed.</p></li>
-	</ul>
-</li>
-<li>[mac_app_link id="443987910" text="App Name"]<p>Displays a text only link to the Mac App</p>
-	<ul>
-	<li>id: The Mac App Store ID<p>This can be found in the copied link from the App Store.<br />For example: <i>https://itunes.apple.com/us/app/1password-password-manager/id<b>443987910</b>?mt=12</i></p></li>
-	<li>text: Link text (optional)<p>If no text is specified, the item name will be displayed.</p></li>
-	</ul>
-</li>
-<li>[itunes_store_link id="62101268" text="iTunes Item Name"]<p>Displays a text only link to the iTunes Item</p>
-	<ul>
-	<li>id: The iTunes Store ID<p>This can be found in the copied link from the iTunes Store.<br />For example: <i>https://itunes.apple.com/us/album/aliens-original-motion-picture/id<b>62101268</b></i><br />Works with Songs, Albums, Movies, Short Films, TV Episodes, Seasons and Music Videos</p></li>
-	<li>text: Link text (optional)<p>If no text is specified, the item name will be displayed.</p></li>
+	<li>id: The app or items's ID</li>
 	</ul>
 </li>
 <li>[amazon_item_link asin="B005F02DA0" linktext="Star Wars Ep. 7" textmode="linktext" mode="text" showprice="yes"]<p>Displays the link as a button or text with the Link Text to the Amazon Item</p>
 	<ul>
-	<li>asin: The Amazon Standard Identification Number is a 10-character alphanumeric unique identifier assigned by Amazon.com<p>Found in the URL from Amazon.com, for example: <i>http://www.amazon.com/dp/<b>B001KNH8VU</b>/?tag=047-20</i></p></li>
+	<li>asin: The Amazon Standard Identification Number</li>
 	<li>linktext: Link text (optional)<p>This will be used for the link if textmode=linktext</p></li>
 	<li>showprice: (optional)<p>If this is set to yes, the price will be shown after the link text</p></li>
 	<li>textmode: [<b>linktext</b>, itemname, defaulttext]
@@ -117,9 +98,9 @@
 <h3 class="asa_admin">Display elements from an item:</h3>
 
 <ul>
-	<li>[ios_app_elements id="568903335" elements="appName"]<p>Displays the element or elements for the iOS or Mac App</p>
+	<li>[asa_elements id="568903335" elements="appName"]<p>Displays the element or elements for the iOS or Mac App</p>
 	<ul>
-	<li>id: The iOS or Mac app's App Store ID<p>This can be found in the copied link from the iTunes App Store.<br />For example: <i>https://itunes.apple.com/us/app/1password/id<b>568903335</b>?mt=8</i></p></li>
+	<li>id: The iOS,  Mac app or iTunes item's ID</li>
 	<li>elements: One or more of the following items:<br />
 	
 <ul>
@@ -141,4 +122,6 @@
 	</ul>
 </li>
 </ul></div>
-
+<?php
+$showSaveChangesButton = false;
+?>
